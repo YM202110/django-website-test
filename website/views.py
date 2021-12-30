410f7e22a700c7ac9cfbb7692e994092465d10e6
y.mori <a.m.h.yu90@gmail.com>
2021-12-30 20:20:07 +0900

    index.htmlとabout.htmlを作成

--- website/views.py
+++ website/views.py
@@ -5,3 +5,6 @@ from django.views.generic import TemplateView
 # Create your views here.
 class IndexView(TemplateView):
     template_name = "index.html"
+
+class AboutView(TemplateView):
+    template_name = "about.html"
